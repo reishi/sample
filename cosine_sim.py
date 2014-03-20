0fde795e097b9a5538b027df2237771621f8ed5e
Reishi Morioka <reishi42@gmail.com>
2014-03-21 01:17:05 +0900

    Add cosine_sim.py

---
+++ cosine_sim.py
@@ -0,0 +1,12 @@
+import math
+
+def scalar(vector):
+    return math.sqrt(sum(x * x for x in vector.values()))
+
+def similarity(a, b):
+    total = sum(a[k] * b.get(k, 0) for k in a)
+    return total / (scalar(a) * scalar(b))
+
+d0 = {'vaio': 1, 'sony': 2, 'thinkpad': 3, 'apple': 4 }
+d1 = {'apple': 1, 'ipod': 2, 'thinkpad': 3, 'sony': 4 }
+print(similarity(d0, d1))
