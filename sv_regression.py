a549a118006b885b95d62192e07548ca9ef85d57
Reishi Morioka <reishi@reishi-imac.local>
2014-06-30 23:40:54 +0900

    Add y_true curve

--- sv_regression.py
+++ sv_regression.py
@@ -3,12 +3,15 @@ from sklearn import svm
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, np.pi * 2.0, 100)
-y_true = np.sin(x) + 0.3 * np.random.randn(x.size)
-plt.scatter(x, y_true)
+y_true = np.sin(x)
+y_train = np.sin(x) + 0.3 * np.random.randn(x.size)
+plt.plot(x, y_true, 'b')
+plt.scatter(x, y_train)
 
 clf = svm.SVR()
 clf.fit([[xv] for xv in x], y_true)
 y_pred = clf.predict([[xv] for xv in x])
+plt.plot(x, y_pred, 'r')
 
-plt.plot(x, y_pred)
+plt.legend(('y_true', 'y_pred'), 'upper right')
 plt.show()
