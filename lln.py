a5e45ebd3a420a45a5acdccff9f8d872559866b2
Reishi Morioka <reishi@reishi-imac.local>
2014-11-21 08:10:17 +0900

    Fix sample mean

--- lln.py
+++ lln.py
@@ -3,17 +3,18 @@ import scipy.stats
 import matplotlib.pyplot as plt
 import math
 
-population = np.random.randn(10000)
+population = np.random.randn(5000)
+plt.plot([0, population.size], [population.mean(), population.mean()], color='r')
 
 x = []
 y = []
 yerr = []
 
-for i in range(10, 3000, 100):
+for i in range(10, 3500, 100):
     sample = np.random.choice(population, size=i)
     sem = sample.std() / math.sqrt(i)
     x.append(i)
-    y.append(sem)
+    y.append(sample.mean())
     yerr.append(2 * sem)
 
 plt.errorbar(x, y, yerr=yerr)
